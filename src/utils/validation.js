4c9efd7c2a69d99545ab26e3ef7591c58c9926fb
Max Pasechnik <maxpasechnik144@gmail.com>
2019-10-17 21:35:03 +0300

    initial commit

---
+++ src/utils/validation.js
@@ -0,0 +1,15 @@
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string('Enter an username')
+    .required('Username is required')
+    .min(4, 'Username must contain more, than 4 digits'),
+  password: Yup.string('Enter a password')
+    .required('Password is required')
+    .min(8, 'Password should  be longer than 8 digits'),
+  passwordConfirm: Yup.string('Enter an author')
+    .required('Password confirmation is required')
+    .oneOf([Yup.ref('password'), null], 'Passwords should match'),
+});
+
+export default validationSchema;
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+require('@babel/polyfill');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: ['@babel/polyfill', './src'],
+  mode: 'development',
+  resolve: { extensions: ['.js', '.jsx'] },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './js/dist',
+    hot: true,
+    historyApiFallback: true,
+    port: 3000,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.m?jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/react'],
+          },
+        },
+      },
+    ],
+  },
+};
